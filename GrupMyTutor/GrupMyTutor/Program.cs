0fe85dffa67e7702543da9ea34bfdd47a18307b2
Muhammad Johan Alibasa <m.johan.alibasa@gmail.com>
2021-06-17 17:14:11 +1000

    change the behaviour to accept input from users

--- GrupMyTutor/GrupMyTutor/Program.cs
+++ GrupMyTutor/GrupMyTutor/Program.cs
@@ -6,8 +6,8 @@ namespace GrupMyTutor
     {
         static void Main(string[] args)
         {
-            int johan = 0;
-            Console.WriteLine("Hello Johan!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
         }
     }
 }
