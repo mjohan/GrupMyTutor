a1bcf9eeac421e1674e90d8ec1356431be8bf246
Muhammad Johan Alibasa <m.johan.alibasa@gmail.com>
2021-06-17 17:04:57 +1000

    setup initial project

---
+++ GrupMyTutor/GrupMyTutor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrupMyTutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
